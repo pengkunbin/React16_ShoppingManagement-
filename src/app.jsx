cd4341f7a458b9dd29f25dc06ce94487a6d71d38
jizou <815371297@qq.com>
2019-04-27 15:25:31 +0800

    commit

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+// console.log("helloword")
+
+// let a =123;
+// let test = ()=>{
+//     return a * 2;
+// }
+// test(a);
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import './index.css'
+ReactDom.render(
+    <h1>Hello,world!</h1>,
+    document.getElementById('app')
+)
